f48ecaae632eaf491d2dcf9021134e0aa08c4b4b
jlziegler <josh.ziegler@icloud.com>
2021-04-29 13:52:43 +0200

    import txt

--- euclidian_distance.R
+++ euclidian_distance.R
@@ -1,10 +1,22 @@
 
 
+# setup
+library(tidyverse)
+
 # load formants
+# DORIE
+formants_dorie <- read.table("formants_dorie.txt")
+colnames(formants_dorie) <- as.matrix(formants_dorie[1,])
+formants_dorie <- formants_dorie[-1,]
+# NEMO
+formants_nemo <- read.table("formants_nemo.txt")
+colnames(formants_nemo) <- as.matrix(formants_nemo[1,])
+formants_nemo <- formants_nemo[-1,]
 
 # Hz to Bark
 library(emuR)
-#bark()
+# DORIE
+formants_dorie$F1_mean.Hz. <- 
 
 # calculate mean and sd of F1, F2 and F3
 
