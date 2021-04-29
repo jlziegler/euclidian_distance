4c267a66e207e3a9d437083e2703b4af80d94f3c
jlziegler <josh.ziegler@icloud.com>
2021-04-29 14:23:07 +0200

    mean & sd tables

--- euclidian_distance.R
+++ euclidian_distance.R
@@ -3,11 +3,15 @@
 # setup
 library(tidyverse)
 
-# load formants
+# load formant tables
 # DORIE
 formants_dorie <- read.table("formants_dorie.txt", header = T)
 # NEMO
 formants_nemo <- read.table("formants_nemo.txt", header = T)
+
+# create vowel column
+formants_dorie$vowel <- gsub("[0-9]", "", formants_dorie$Label)
+formants_nemo$vowel <- gsub("[0-9]", "", formants_nemo$Label)
 
 # Hz to Bark
 library(emuR)
@@ -21,7 +25,23 @@ formants_nemo$F1_mean.Bark <- bark(formants_nemo$F1_mean.Hz.)
 formants_nemo$F2_mean.Bark <- bark(formants_nemo$F2_mean.Hz.)
 formants_nemo$F3_mean.Bark <- bark(formants_nemo$F3_mean.Hz.)
 
-# calculate mean and sd of F1, F2 and F3
+# calculate mean and sd of F1, F2 and F3 Bark
+# mean & sd DORIE
+meansd_dorie <- formants_dorie %>%
+  select_at(vars("vowel", "F1_mean.Bark", "F2_mean.Bark", "F3_mean.Bark")) %>%
+  gather(key = 'formant', value = "value", -vowel) %>%
+    group_by(vowel, formant) %>%
+    summarise(mean = mean(value), sd = sd(value))
 
+# mean & sd NEMO
+meansd_nemo <- formants_nemo %>%
+  select_at(vars("vowel", "F1_mean.Bark", "F2_mean.Bark", "F3_mean.Bark")) %>%
+  gather(key = 'formant', value = "value", -vowel) %>%
+  group_by(vowel, formant) %>%
+  summarise(mean = mean(value), sd = sd(value))
 
 # Euclidian Distance
+
+
+
+
