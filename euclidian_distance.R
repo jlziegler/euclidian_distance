6b1d47e0fa8e072735d5a53aa53b5ff98be3e9f4
jlziegler <josh.ziegler@icloud.com>
2021-04-29 13:56:08 +0200

    hz to bark

--- euclidian_distance.R
+++ euclidian_distance.R
@@ -5,14 +5,21 @@ library(tidyverse)
 
 # load formants
 # DORIE
-formants_dorie <- read.table("formants_dorie.txt")
+formants_dorie <- read.table("formants_dorie.txt", header = T)
 # NEMO
 formants_nemo <- read.table("formants_nemo.txt", header = T)
 
 # Hz to Bark
 library(emuR)
 # DORIE
-formants_dorie$F1_mean.Bark <- 
+formants_dorie$F1_mean.Bark <- bark(formants_dorie$F1_mean.Hz.)
+formants_dorie$F2_mean.Bark <- bark(formants_dorie$F2_mean.Hz.)
+formants_dorie$F3_mean.Bark <- bark(formants_dorie$F3_mean.Hz.)
+
+# NEMO
+formants_nemo$F1_mean.Bark <- bark(formants_nemo$F1_mean.Hz.)
+formants_nemo$F2_mean.Bark <- bark(formants_nemo$F2_mean.Hz.)
+formants_nemo$F3_mean.Bark <- bark(formants_nemo$F3_mean.Hz.)
 
 # calculate mean and sd of F1, F2 and F3
 
